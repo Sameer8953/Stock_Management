1f70a340932dcf4002d99d30417beea97530c81b
sameer8953 <sameer.ahmadid8953@gmail.com>
2024-09-02 10:13:07 +0530

    7th Day Login Function Implements

--- Controller/AccountController.cs
+++ Controller/AccountController.cs
@@ -7,6 +7,7 @@ using API.Interfaces;
 using API.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controller
 {
@@ -16,14 +17,42 @@ namespace API.Controller
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager , ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager , ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoggingDto loginDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x =>x.UserName == loginDto.Username.ToLower());
+
+            if (user == null)
+                return Unauthorized("Invalid Username !");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user , loginDto.Password, false);
+
+            if(!result.Succeeded) return Unauthorized("Username  not found and / or password incorrect");
+
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = user.UserName,
+                    Email =  user.Email,
+                    Token = _tokenService.CreateToken(user)
+                }
+            );
+                
+            
         }
 
         [HttpPost("register")]
-
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
             try
@@ -44,7 +73,7 @@ namespace API.Controller
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
                     if (roleResult.Succeeded)
                     {
-#pragma warning disable CS8601 // Possible null reference assignment.
+
                         return Ok(
                             new NewUserDto
                             {
@@ -53,7 +82,7 @@ namespace API.Controller
                                 Token = _tokenService.CreateToken(appUser)
                             }
                         );
-#pragma warning restore CS8601 // Possible null reference assignment.
+
                     }
                     else
                     {
